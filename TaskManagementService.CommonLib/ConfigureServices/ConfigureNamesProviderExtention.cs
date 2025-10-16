56d98ce4e1fe282dadf9188b8aa9d14198175441
Sergey Poleshchuk <sergey.poleschuk@leomax.ru>
2025-10-16 20:54:43 +0300

    fix

--- TaskManagementService.CommonLib/ConfigureServices/ConfigureNamesProviderExtention.cs
+++ TaskManagementService.CommonLib/ConfigureServices/ConfigureNamesProviderExtention.cs
@@ -5,13 +5,6 @@ namespace TaskManagementService.CommonLib.ConfigureServices;
 
 public static class ConfigureNamesProviderExtention
 {
-    /// <summary>
-    /// Пример использования: 
-    /// services.AddSystemNames(names => { names.CompanyNamespace = "Shop24"; names.ServiceNamespace = "Orders"; names.ServiceName = "Orders"; names.RoutePrefix = "orders"; });
-    /// </summary>
-    /// <param name="services"></param>
-    /// <param name="configureNames"></param>
-    /// <returns></returns>
     public static IServiceCollection AddSystemNames(this IServiceCollection services, Action<NamesProvider> configureNames)
     {
         var names = new NamesProvider();
